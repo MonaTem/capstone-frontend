e844195cecdee79ecf2c4646a9083efb5bcfcf8b
Mona Temchin <monatemchin@gmail.com>
2018-04-02 22:38:59 -0400

    Refactored Fabia.js, removed Reactstrap
    Refactored Fabia.js, rmeoved Reactstrap, temporarily removed VoicePlayer and fetch from Fabia.js
    Will add back in later.

--- src/components/Fabia.js
+++ src/components/Fabia.js
@@ -1,127 +1,79 @@
 import React, { Component } from 'react';
 import './Fabia.css';
 import VoicePlayer from './VoicePlayer.js';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, ButtonGroup } from 'reactstrap';
-
-
 
 class Fabia extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      character: 'Bear',
-      chars: ['Bear', 'Princess', 'Witch', 'Woodcutter', 'Fairy Queen', 'Ogre'],
       dropdownOpen: false,
-      rSelected: '',
       voice: null,
       stories: '',
-      id: 1,
       api: '/api/stories/1/happy_story',
-      ending: ''
+      selected: "Bear",
     }
 
-    this.select  = this.select.bind(this);
+    this.handleSelect  = this.handleSelect.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.toggle = this.toggle.bind(this);
+
   }
 
-  // componentDidMount() {
-  //   const api = this.state.api;
-  //   console.log(`URL is ${api}`);
-  //   fetch(api)
-  //     .then(response => response.text())
-  //     .then(stories => this.setState({ stories }));
-  // }
 
-    toggle() {
+    handleSelect(event) {
+      const name = event.target.name;
+      const value= event.target.value;
       this.setState({
-        dropdownOpen: !this.state.dropdownOpen
+        [name]: value,
+        dropdownOpen: !this.state.dropdownOpen,
       });
+
     }
 
-    select(event) {
+    handleClick(ending, event) {
       event.preventDefault();
       this.setState({
-        character: event.target.innerText,
-        dropdownOpen: !this.state.dropdownOpen,
-        voice: null
+        ending: ending,
+        // api: `/api/stories/1/${this.state.ending}_story`
       });
-
-    }
-
-    handleClick(event) {
-      // console.log("rSelected is " + this.state.rSelected);
-      // let selectEnd = event.target.innerText;
-      // console.log('selected ending is ' + selectEnd);
-
-      //let api = this.state.api;
-      this.setState({
-        rSelected: event.target.innerText,
-        // rSelected: 'Sad ending?',
-        id: (this.state.chars.indexOf(this.state.rSelected) + 1),
-        ending: this.state.rSelected.toLowerCase().substring(0,this.state.rSelected.length-8),
-        api: `/api/stories/1/${this.state.ending}_story`
-      });
-      console.log("rSelected is " + this.state.rSelected);
-      console.log("state ending is " + this.state.ending);
-      console.log("state api is " + this.state.api);
-      let api = this.state.api;
-      // this.state.ending === 'Sad' || 'sad' ? (api = '/api/stories/1/sad_story') : (api = '/api/stories/1/happy_story');
-      console.log("api is " + api);
-      fetch(api)
-        .then(response => response.text())
-        .then(stories => this.setState({ stories,
-                                         voice: (<VoicePlayer
-                                                  play
-                                                  text={this.state.stories}
-                                                 />)
-                                        }));
 
     }
 
 
   render() {
     const white = 'white';
-
+    const ending = this.state.ending;
+    const character = this.state.selected;
+    console.log("ending is: ", ending, "character is: ", character)
 
     return (
-
       <div>
         <h5 className="h5">
-            Hello, My Name is Fabia and I'd Love to Tell You a Fairytale
+            Hello, My Name is Fabia and I would Love to Tell You a Fairytale
         </h5>
 
         <div className="Fabia">
           <img className="img-fluid" src="/AdobeStock_59629549_Preview.jpeg" alt="Purple Fairy" height="300" width="300"/>
         </div>
-        <Dropdown className="dropit" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-           <DropdownToggle color={white} caret>
-           Choose Main Character
-          </DropdownToggle>
-          <DropdownMenu className="menu">
-            <DropdownItem onClick={this.select.bind(this)}>Bear</DropdownItem>
-            <DropdownItem onClick={this.select.bind(this)}>Princess</DropdownItem>
-            <DropdownItem onClick={this.select.bind(this)}>Woodcutter</DropdownItem>
-            <DropdownItem onClick={this.select.bind(this)}>Witch</DropdownItem>
-            <DropdownItem onClick={this.select.bind(this)}>Fairy Queen</DropdownItem>
-            <DropdownItem onClick={this.select.bind(this)}>Ogre</DropdownItem>
-         </DropdownMenu>
-        </Dropdown>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <ButtonGroup className="btngrp">
-          <Button color="white" onClick={this.handleClick.bind(this)}>Happy ending?</Button>
-          <Button color="white" onClick={this.handleClick.bind(this)}>Sad ending?</Button>
-        </ButtonGroup>
-        <p>Selected: {this.state.rSelected.substring(0,this.state.rSelected.length-1)}</p>
-      {this.state.voice}
-</div>
+        <form onSubmit={this.handleClick}>
+        <label>
+           Choose Main Character:
+           <br/>
+           <select name="selected" value={this.state.value} onChange={this.handleSelect}>
+             <option value="Bear">Bear</option>
+             <option value="Princess">Princess</option>
+             <option value="Witch">Witch</option>
+             <option value="Woodcutter">Woodcutter</option>
+             <option value="Fairy">Fairy</option>
+             <option value="Ogre">Ogre</option>
+           </select>
+        </label>
+
+        <br/><br/>
+          <button color="white" onClick={this.handleClick.bind(this, "Happy Ending")}>Happy ending?</button>
+          <button name="ending" value="sad" color="white" onClick={this.handleClick.bind(this, "Sad Ending")}>Sad ending?</button>
+      </form>
+      <p>Selected: {character}, {ending}</p>
+ </div>
     );
   }
 }
