613d801b1798c8c6ef15479ea9a626656e898ecd
Mona Temchin <monatemchin@gmail.com>
2018-04-13 21:14:15 -0400

    Added conditional rendering for bear and everyone else

--- src/components/Fabia.js
+++ src/components/Fabia.js
@@ -85,21 +85,64 @@ class Fabia extends Component {
 
     }
 
+/*
+  renderCharacter() {
+    switch (character) {
+      case: "Bear":
+
+    }
+  }
+*/
+
 
   render() {
     const ending = this.state.ending;
     const character = this.state.selected;
     console.log("ending is: ", ending, "character is: ", character);
 
-    return (
-      <div>
-        <h5 className="h5">
+    if (character === "Bear") {
+       return (
+           <div>
+             <h5 className="h5">
+              I'm the Bear and This is My Story
+             </h5>
+
+             <div className="Bear">
+              <img className="img-fluid" src="PurpleBear.jpeg" alt="Purple Bear" height="300" width="300"/>
+             </div>
+             <form onSubmit={this.handleClick}>
+             <label>
+                Choose Main Character:
+                <br/>
+                <select name="selected" value={this.state.value} onChange={this.handleSelect}>
+                  <option value="Bear">Bear</option>
+                  <option value="Princess">Princess</option>
+                  <option value="Witch">Witch</option>
+                  <option value="Woodcutter">Woodcutter</option>
+                  <option value="Fairy">Fairy</option>
+                  <option value="Ogre">Ogre</option>
+                </select>
+             </label>
+
+             <br/><br/>
+               <button name="ending" color="white" value="happy"  onClick={this.handleClick.bind(this, "Happy Ending")}>Happy ending?</button>
+               <button name="ending" value="sad" color="white" onClick={this.handleClick.bind(this, "Sad Ending")}>Sad ending?</button>
+           </form>
+           <p>Selected: {character}, {ending}</p>
+           {this.state.voice}
+          </div>
+       )
+    } else {
+        return (
+         <div>
+          <h5 className="h5">
             Hello, My Name is Fabia and I would Love to Tell You a Fairytale
-        </h5>
+         </h5>
 
         <div className="Fabia">
           <img className="img-fluid" src="/AdobeStock_59629549_Preview.jpeg" alt="Purple Fairy" height="300" width="300"/>
         </div>
+
         <form onSubmit={this.handleClick}>
         <label>
            Choose Main Character:
@@ -118,10 +161,11 @@ class Fabia extends Component {
           <button name="ending" color="white" value="happy"  onClick={this.handleClick.bind(this, "Happy Ending")}>Happy ending?</button>
           <button name="ending" value="sad" color="white" onClick={this.handleClick.bind(this, "Sad Ending")}>Sad ending?</button>
       </form>
-      <p>Selected: {character}, {ending}</p>
+      <p>Selected: {character}  {ending}</p>
       {this.state.voice}
- </div>
+     </div>
     );
+   }
   }
 }
 
