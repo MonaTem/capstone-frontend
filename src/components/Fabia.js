8f303c2a66c38cdb8378bb35576f52253ceac3c9
Mona Temchin <monatemchin@gmail.com>
2018-04-13 22:02:45 -0400

    Conditional rendering code refactored into separate render methods, one for character, one for form

--- src/components/Fabia.js
+++ src/components/Fabia.js
@@ -16,6 +16,8 @@ class Fabia extends Component {
 
     this.handleSelect  = this.handleSelect.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.renderCharacter = this.renderCharacter.bind(this);
+    this.renderForm = this.renderForm.bind(this);
 
   }
 
@@ -85,87 +87,78 @@ class Fabia extends Component {
 
     }
 
-/*
-  renderCharacter() {
-    switch (character) {
-      case: "Bear":
 
-    }
+    renderCharacter() {
+      const character = this.state.selected;
+      if (character === 'Bear') {
+         return (
+             <div>
+               <h5 className="h5">
+                   I'm the Bear and This is My Story
+               </h5>
+
+               <div className="Bear">
+                    <img className="img-fluid" src="PurpleBear.jpeg" alt="Purple Bear" height="300" width="300"/>
+               </div>
+            </div>
+         );
+
+       } else {
+          return (
+            <div>
+              <h5 className="h5">
+                  Hello, My Name is Fabia and I would Love to Tell You a Fairytale
+              </h5>
+
+              <div className="Fabia">
+                   <img className="img-fluid" src="/AdobeStock_59629549_Preview.jpeg" alt="Purple Fairy" height="300" width="300"/>
+              </div>
+            </div>
+         );
+       }
   }
-*/
 
+   renderForm() {
 
-  render() {
-    const ending = this.state.ending;
-    const character = this.state.selected;
-    console.log("ending is: ", ending, "character is: ", character);
+     const ending = this.state.ending;
+     const character = this.state.selected;
 
-    if (character === "Bear") {
-       return (
-           <div>
-             <h5 className="h5">
-              I'm the Bear and This is My Story
-             </h5>
-
-             <div className="Bear">
-              <img className="img-fluid" src="PurpleBear.jpeg" alt="Purple Bear" height="300" width="300"/>
-             </div>
-             <form onSubmit={this.handleClick}>
-             <label>
-                Choose Main Character:
-                <br/>
-                <select name="selected" value={this.state.value} onChange={this.handleSelect}>
-                  <option value="Bear">Bear</option>
-                  <option value="Princess">Princess</option>
-                  <option value="Witch">Witch</option>
-                  <option value="Woodcutter">Woodcutter</option>
-                  <option value="Fairy">Fairy</option>
-                  <option value="Ogre">Ogre</option>
-                </select>
-             </label>
-
-             <br/><br/>
-               <button name="ending" color="white" value="happy"  onClick={this.handleClick.bind(this, "Happy Ending")}>Happy ending?</button>
-               <button name="ending" value="sad" color="white" onClick={this.handleClick.bind(this, "Sad Ending")}>Sad ending?</button>
-           </form>
-           <p>Selected: {character}, {ending}</p>
-           {this.state.voice}
-          </div>
-       )
-    } else {
-        return (
-         <div>
-          <h5 className="h5">
-            Hello, My Name is Fabia and I would Love to Tell You a Fairytale
-         </h5>
-
-        <div className="Fabia">
-          <img className="img-fluid" src="/AdobeStock_59629549_Preview.jpeg" alt="Purple Fairy" height="300" width="300"/>
-        </div>
-
-        <form onSubmit={this.handleClick}>
+     return (
+      <div>
+       <form onSubmit={this.handleClick}>
         <label>
-           Choose Main Character:
-           <br/>
-           <select name="selected" value={this.state.value} onChange={this.handleSelect}>
-             <option value="Bear">Bear</option>
-             <option value="Princess">Princess</option>
-             <option value="Witch">Witch</option>
-             <option value="Woodcutter">Woodcutter</option>
-             <option value="Fairy">Fairy</option>
-             <option value="Ogre">Ogre</option>
-           </select>
+         Choose Main Character:
+        <br/>
+        <select name="selected" value={this.state.value} onChange={this.handleSelect}>
+         <option value="Bear">Bear</option>
+         <option value="Princess">Princess</option>
+         <option value="Witch">Witch</option>
+         <option value="Woodcutter">Woodcutter</option>
+         <option value="Fairy">Fairy</option>
+         <option value="Ogre">Ogre</option>
+        </select>
         </label>
 
         <br/><br/>
-          <button name="ending" color="white" value="happy"  onClick={this.handleClick.bind(this, "Happy Ending")}>Happy ending?</button>
-          <button name="ending" value="sad" color="white" onClick={this.handleClick.bind(this, "Sad Ending")}>Sad ending?</button>
-      </form>
-      <p>Selected: {character}  {ending}</p>
-      {this.state.voice}
-     </div>
+         <button name="ending" color="white" value="happy"  onClick={this.handleClick.bind(this, "Happy Ending")}>Happy ending?</button>
+         <button name="ending" value="sad" color="white" onClick={this.handleClick.bind(this, "Sad Ending")}>Sad ending?</button>
+       </form>
+       <p>Selected: {character}, {ending}</p>
+      </div>
     );
    }
+
+
+  render() {
+
+    // console.log("ending is: ", ending, "character is: ", character);
+    return (
+      <div>
+       {this.renderCharacter()}
+       {this.renderForm()}
+       {this.state.voice}
+      </div>
+    );
   }
 }
 
