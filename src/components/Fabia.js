c3ecdfc5e3c28ba8ce6b08011ca1c2db712586a1
Mona Temchin <monatemchin@gmail.com>
2018-04-15 13:34:48 -0400

    Change url to production to prepare for push to Heroku

--- src/components/Fabia.js
+++ src/components/Fabia.js
@@ -75,8 +75,10 @@ class Fabia extends Component {
     const characterIndex = array.indexOf(this.state.selected) + 1;
     console.log("end: ", end, "characterInd:", characterIndex);
     // const url = "/api/stories/" + characterIndex + "/" + end  This is URL for fake server
-    //const HerokuUrl = "https://fabias-fairytales-backend.herokuapp.com";
-    const HerokuUrl = "http://localhost:8000";
+    // This is production url for fetch---comment out in development
+    const HerokuUrl = "https://fabias-fairytales-backend.herokuapp.com";
+    // This is development url for fetch--comment out for production
+    //const HerokuUrl = "http://localhost:8000";
     const url = HerokuUrl + "/api/stories/" + end + "/" + characterIndex;
     console.log("url", url);
     const stories = '';
@@ -105,7 +107,7 @@ class Fabia extends Component {
          Choose Main Character:
         <br/>
         <select name="selected" value={this.state.value} onChange={this.handleSelect}>
-         <option value=""> </option>  
+         <option value=""> </option>
          <option value="Bear">Bear</option>
          <option value="Princess">Princess</option>
          <option value="Witch">Witch</option>
